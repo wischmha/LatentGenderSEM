27c6148f49a3d48e49f2c88fcdcc2842b219ca76
Hans-Aloys Wischmann <159247424+wischmha@users.noreply.github.com>
2026-02-25 14:32:14 +0100

    Resubmission with Sensitivity Analysis

--- Config/Domain_Knowledge_SEM.R
+++ Config/Domain_Knowledge_SEM.R
@@ -1,6 +1,6 @@
 # Charité - Universitätsmedizin Berlin, Institut für Public Health
 # Mascha Kern
-# March 26, 2025
+# February 23, 2026
 
 # Common outcome regressions: Potential pre-Dx risk factors and pre-Dx behavior(s)
 m_dx <- "
@@ -19,7 +19,7 @@ sem_measurement_model <- "
 "
 
 sem_regressions_model <- "
-  # hypothesis: sex at birth -> gender, fix scale for gender, comparable to sex_binary
+  # hypothesis: sex at birth -> gender, fix variance for gender, comparable dynamic range to sex_binary
   gender ~ 1.0 * sex_binary
 
   # regressions between other exogeneous variables and gender predictors
@@ -47,6 +47,7 @@ sem_model_wo_age <-
         "group: 3\n", sem_measurement_model_wo_age, sem_regressions_model_wo_age, m_dx_wo_age, s_dx_wo_age, "\n",
         "group: 2\n", sem_measurement_model_wo_age, sem_regressions_model_wo_age, m_dx_wo_age, s_dx_wo_age, "\n",
         "group: 1\n", sem_measurement_model_wo_age, sem_regressions_model_wo_age, m_dx_wo_age, s_dx_wo_age, sep = "")
+sem_model_sens <- str_replace_all(sem_model, c("gender.*~.*1.0.*[*].*sex_binary" = "gender ~~ 1.0 * gender"))
 
 # GDM model (logistic regression for sex_binary, same predictors as in SEM for gender, including 2nd level predictors)
 gdm_model <- "
@@ -54,3 +55,19 @@ gdm_model <- "
                             (current_monthly_gross_labor_income + gross_hourly_wage) * east_german_residence + employment_status_imp +
                             highest_educational_degree + risk_taking_scale + political_interest + current_mat_parent_leave + num_physician_visits)
 "
+
+# extract nodes from main SEM, and manually specify clean layout for plot
+lavaan_sem <- lavaanify(sem_model)
+sem_nodes  <- setdiff(unique(c(lavaan_sem$lhs, lavaan_sem$rhs)), "gender")
+sem_layout <- get_layout(
+  "num_children_in_household", "risk_taking_scale", "num_physician_visits",
+  "political_interest", "employment_status_imp", "gross_hourly_wage", "",
+  "", "daily_hours_childcare_weekdays", "", "", "", "highest_educational_degree", "east_german_residence",
+  "", "daily_hours_housework_weekdays", "", "gender", "", "current_monthly_gross_labor_income", "",
+  "partner", "current_mat_parent_leave", "", "sex_binary", "", "", "age_10y",
+  "", "", "", "", "", "", "",
+  "", "migraine_incidence", "", "sex_or", "", "stroke_incidence", "",
+  "", "", "", "immigration_history", "", "", "",
+  "smoke_before_migraine", "diabetes_before_migraine", "hypertension_before_migraine",
+  "", "hypertension_before_stroke", "diabetes_before_stroke", "smoke_before_stroke",
+  rows = 8)
